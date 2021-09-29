e08209c415acb72d3e1983c812e95c662caee293
KJH <kjhg5852@naver.com>
2021-09-29 15:58:21 +0900

    Hello World

--- main.go
+++ main.go
@@ -1,17 +1,27 @@
 package main
 
-import (
-	"github.com/gin-gonic/gin"
-)
+import "github.com/gin-gonic/gin"
 
 func main() {
 	r := gin.Default()
 
-	r.GET("/ping", func(c *gin.Context) {
+	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "pong",
+			"message": "Hello World",
 		})
 	})
+
+	api := r.Group("/api")
+	{
+		v1 := api.Group("/v1")
+		{
+			v1.GET("/ping", func(c *gin.Context) {
+				c.JSON(200, gin.H{
+					"message": "pong",
+				})
+			})
+		}
+	}
 
 	r.Run(":4000")
 }
